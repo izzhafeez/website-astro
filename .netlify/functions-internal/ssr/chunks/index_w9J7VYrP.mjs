e44999dce3d5e91220b801c2ed76edb237c67eb3
Izz Hafeez <izzhafeez@gmail.com>
2024-03-04 20:22:34 +0800

    Remove netlify folder

--- .netlify/functions-internal/ssr/chunks/index_w9J7VYrP.mjs
+++
@@ -1,6 +0,0 @@
-;!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="7b794f06-ded1-4d5c-a890-8130facb9d45",e._sentryDebugIdIdentifier="sentry-dbid-7b794f06-ded1-4d5c-a890-8130facb9d45")}catch(e){}}();export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_j5B9pZYU.mjs').then(n => n.c);
-
-export { page };
-//# sourceMappingURL=index_w9J7VYrP.mjs.map
--- .netlify/functions-internal/ssr/chunks/index_xcBdqH65.mjs
+++
@@ -1,6 +0,0 @@
-;!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="2c22d22a-4cbc-4427-9db6-37d52bc6aeef",e._sentryDebugIdIdentifier="sentry-dbid-2c22d22a-4cbc-4427-9db6-37d52bc6aeef")}catch(e){}}();export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_j5B9pZYU.mjs').then(n => n.b);
-
-export { page };
-//# sourceMappingURL=index_xcBdqH65.mjs.map
