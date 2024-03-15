ddae9f88e2760a1481c0006991573561571965df
Izz Hafeez <izzhafeez@gmail.com>
2024-03-15 17:40:40 +0800

    Edit components: Rename to quizzes

--- src/components/gqmaster/map/featureStore.js
+++
@@ -1,6 +0,0 @@
-import { writable } from "svelte/store";
-
-export const toRemove = writable(null);
-export const toAdd = writable(null);
-export const toHideTooltip = writable(null);
-export const toShowTooltip = writable(null);
