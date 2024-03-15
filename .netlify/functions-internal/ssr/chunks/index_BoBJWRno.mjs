8297bf4e36f6d580639ce2b693df9224996b6117
Izz Hafeez <izzhafeez@gmail.com>
2024-03-15 16:54:42 +0800

    Edit index_BoBJWRno.mjs: Add london underground

---
+++ .netlify/functions-internal/ssr/chunks/index_BoBJWRno.mjs
@@ -0,0 +1,6 @@
+;!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="1ca8d398-6695-4bcb-a30d-6731efbaa339",e._sentryDebugIdIdentifier="sentry-dbid-1ca8d398-6695-4bcb-a30d-6731efbaa339")}catch(e){}}();export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_C8u2KI10.mjs').then(n => n.h);
+
+export { page };
+//# sourceMappingURL=index_BoBJWRno.mjs.map
