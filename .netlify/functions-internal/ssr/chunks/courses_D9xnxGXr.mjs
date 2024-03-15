76dab7847eac87568796d06ee60ce1afc2b85655
Izz Hafeez <izzhafeez@gmail.com>
2024-03-15 16:54:42 +0800

    Edit courses_D9xnxGXr.mjs: Add london underground

---
+++ .netlify/functions-internal/ssr/chunks/courses_D9xnxGXr.mjs
@@ -0,0 +1,6 @@
+;!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="ae1ab682-8fe3-4007-985d-b3c237994090",e._sentryDebugIdIdentifier="sentry-dbid-ae1ab682-8fe3-4007-985d-b3c237994090")}catch(e){}}();export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_C8u2KI10.mjs').then(n => n.e);
+
+export { page };
+//# sourceMappingURL=courses_D9xnxGXr.mjs.map
