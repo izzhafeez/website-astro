e50c99ad24ec52ed05414c85bf7fb288587eabc8
Izz Hafeez <izzhafeez@gmail.com>
2024-03-15 16:54:43 +0800

    Edit middleware.mjs: Add london underground

---
+++ .netlify/functions-internal/ssr/middleware.mjs
@@ -0,0 +1,5 @@
+;!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="330a88c2-e6db-4650-8c00-606fec7f9170",e._sentryDebugIdIdentifier="sentry-dbid-330a88c2-e6db-4650-8c00-606fec7f9170")}catch(e){}}();import '@astrojs/internal-helpers/path';
+import 'cookie';
+export { onRequest } from './_astro-internal_middleware.mjs';
+import '@sentry/astro/middleware';
+//# sourceMappingURL=middleware.mjs.map
