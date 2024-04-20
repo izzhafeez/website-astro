a316ea5ac8acd4329531c3ccf6d56789922c2bd1
Izz Hafeez <izzhafeez@gmail.com>
2024-04-20 19:35:52 +0800

    Commit src/components/games/stat-attack/StatAttackRoomCreate.jsx

--- src/components/games/stat-attack/StatAttackRoomCreate.jsx
+++ src/components/games/stat-attack/StatAttackRoomCreate.jsx
@@ -7,17 +7,19 @@ function StatAttackRoomCreate({ title, instructions }) {
   return (
     <div className="p-2 max-w-6xl mx-auto">
       <div className="mt-40 text-center">
-        <h1 className="animate-linear bg-[length:200%_auto] bg-gradient-to-r from-dt-500 to-dt-300 dark:from-dt-500 text-transparent bg-clip-text text-6xl font-extrabold my-4 inline-block">Stat Attack</h1>
+        <h1 className="animate-linear bg-[length:200%_auto] bg-gradient-to-r from-dt-500 to-dt-300 text-transparent bg-clip-text text-6xl font-extrabold my-4 inline-block">Stat Attack</h1>
         <p className="max-w-3xl mx-auto mb-4 ">{gamesData['stat-attack'].heroText} {instructions} Deck: <span className="text-dt-500 dark:text-dt-200 font-bold">{title}</span></p>
-        <input
-          name="gameId"
-          type="text"
-          value={gameId}
-          onChange={e => setGameId(e.target.value)}
-          placeholder='Enter Game ID...'
-          className="transition duration-500 bg-white dark:bg-gray-700 rounded-md me-2"/>
-        <a href={`${window.location.href}/${gameId}`} type="button" className="me-2 p-2 rounded-md bg-dt-500 dark:bg-dt-200 dark:text-black hover:opacity-80 text-white">Create Game</a>
-        <a href={`${window.location.href}/${gameId}`} type="button" className="p-2 rounded-md bg-dt-500 dark:bg-dt-200 dark:text-black hover:opacity-80 text-white">Join Game</a>
+        <div className="flex flex-wrap place-content-center">
+          <input
+            name="gameId"
+            type="text"
+            value={gameId}
+            onChange={e => setGameId(e.target.value)}
+            placeholder='Enter Game ID...'
+            className="transition duration-500 bg-white dark:bg-gray-700 rounded-md me-2"/>
+          <a href={`${window.location.href}/${gameId}`} type="button" className="me-2 p-2 rounded-md bg-dt-500 dark:bg-dt-200 dark:text-black hover:opacity-80 text-white">Create Game</a>
+          <a href={`${window.location.href}/${gameId}`} type="button" className="p-2 rounded-md bg-dt-500 dark:bg-dt-200 dark:text-black hover:opacity-80 text-white">Join Game</a>
+        </div>
       </div>
     </div>
   );
