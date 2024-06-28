07ada7bd0dba2a76264cf8e2795f7e1b20af536c
Izz Hafeez <izzhafeez@gmail.com>
2024-06-28 23:07:32 +0800

    Fix SVG

--- src/components/apps/routeweaver/RouteWeaver.vue
+++ src/components/apps/routeweaver/RouteWeaver.vue
@@ -4,9 +4,30 @@
     <p class="text-lg my-6">A simple route planner that helps you find the best route to visit multiple locations.</p>
     <div class="flex gap-2 my-2">
       <input type="number" v-model="numOfRoutes" class="px-2 py-1 bg-gray-500/20 rounded-md" placeholder="Number of routes"/>
-      <button @click="handleNewLocation" class="px-2 py-1 flex gap-2 bg-black hover:bg-black/80 dark:bg-white dark:hover:bg-white/80 rounded-md"><span class="invert my-auto">Add</span> <img src="/img/common/plus.svg" class="h-4 w-4 invert dark:invert-0 my-auto"/></button>
-      <button @click="() => generateStartingRoute()" class="px-2 py-1 flex gap-2 bg-gray-500/20 hover:bg-gray-500/30 rounded-md"><span class="my-auto">Find Interest Points</span> <img src="/img/common/location.svg" class="h-4 w-4 dark:invert my-auto"/></button>
-      <button @click="() => getBestTravellingSalesman(100)" class="px-2 py-1 flex gap-2 bg-gray-500/20 hover:bg-gray-500/30 rounded-md"><span class="my-auto">Recommend Route</span> <img src="/img/common/route.svg" class="h-4 w-4 dark:invert my-auto"/></button>
+      <button @click="handleNewLocation" class="px-2 py-1 flex gap-2 bg-black hover:bg-black/80 dark:bg-white dark:hover:bg-white/80 rounded-md"><span class="invert my-auto">Add</span> 
+        <svg class="h-4 w-4 invert dark:invert-0 my-auto" width="800px" height="800px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-466.000000, -1089.000000)" fill="#000000">
+                    <path d="M488,1106 L483,1106 L483,1111 C483,1111.55 482.553,1112 482,1112 C481.447,1112 481,1111.55 481,1111 L481,1106 L476,1106 C475.447,1106 475,1105.55 475,1105 C475,1104.45 475.447,1104 476,1104 L481,1104 L481,1099 C481,1098.45 481.447,1098 482,1098 C482.553,1098 483,1098.45 483,1099 L483,1104 L488,1104 C488.553,1104 489,1104.45 489,1105 C489,1105.55 488.553,1106 488,1106 L488,1106 Z M482,1089 C473.163,1089 466,1096.16 466,1105 C466,1113.84 473.163,1121 482,1121 C490.837,1121 498,1113.84 498,1105 C498,1096.16 490.837,1089 482,1089 L482,1089 Z" id="plus-circle" sketch:type="MSShapeGroup"></path>
+                </g>
+            </g>
+        </svg>
+      </button>
+      <button @click="() => generateStartingRoute()" class="px-2 py-1 flex gap-2 bg-gray-500/20 hover:bg-gray-500/30 rounded-md"><span class="my-auto">Find Interest Points</span>
+        <svg class="h-4 w-4 dark:invert my-auto" width="800px" height="800px" viewBox="-4 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+          <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+              <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-106.000000, -413.000000)" fill="#000000">
+                  <path d="M118,422 C116.343,422 115,423.343 115,425 C115,426.657 116.343,428 118,428 C119.657,428 121,426.657 121,425 C121,423.343 119.657,422 118,422 L118,422 Z M118,430 C115.239,430 113,427.762 113,425 C113,422.238 115.239,420 118,420 C120.761,420 123,422.238 123,425 C123,427.762 120.761,430 118,430 L118,430 Z M118,413 C111.373,413 106,418.373 106,425 C106,430.018 116.005,445.011 118,445 C119.964,445.011 130,429.95 130,425 C130,418.373 124.627,413 118,413 L118,413 Z" id="location" sketch:type="MSShapeGroup">
+                  </path>
+              </g>
+          </g>
+        </svg>
+      </button>
+      <button @click="() => getBestTravellingSalesman(100)" class="px-2 py-1 flex gap-2 bg-gray-500/20 hover:bg-gray-500/30 rounded-md"><span class="my-auto">Recommend Route</span>
+        <svg class="h-4 w-4 dark:invert my-auto" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M6 21C7.5 19.4 9 17.9673 9 16.2C9 14.4327 7.65685 13 6 13C4.34315 13 3 14.4327 3 16.2C3 17.9673 4.5 19.4 6 21ZM6 21H17.5C18.8807 21 20 19.8807 20 18.5C20 17.1193 18.8807 16 17.5 16H15M18 11C19.5 9.4 21 7.96731 21 6.2C21 4.43269 19.6569 3 18 3C16.3431 3 15 4.43269 15 6.2C15 7.96731 16.5 9.4 18 11ZM18 11H14.5C13.1193 11 12 12.1193 12 13.5C12 14.8807 13.1193 16 14.5 16H15.6" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </button>
     </div>
   </div>
   <div class="h-80 w-full mt-4">
