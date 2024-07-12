163734b2692ee6fc95a528ea11e937a52e8bd9d2
Izz Hafeez <izzhafeez@gmail.com>
2024-07-12 21:56:00 +0800

    Commit src/components/games/truth-or-dare/TruthDareRoomCreate.tsx

--- src/components/games/truth-or-dare/TruthDareRoomCreate.tsx
+++ src/components/games/truth-or-dare/TruthDareRoomCreate.tsx
@@ -17,22 +17,20 @@ function TruthDareRoomCreate() {
   }
 
   return (
-    <div className="p-2">
-      <div className="p-4 md:p-6 rounded-3xl shadow-md max-w-3xl mt-60 mx-auto bg-ew-500/50">
-      <div className="text-left lg:text-center">
-        <h1 className="text-white text-3xl lg:text-6xl font-extrabold my-4 inline-block">Truth or Dare</h1>
-        <p className="max-w-3xl mb-4 text-white">{gamesData['truth-or-dare'].heroText} <span className="underline hover:text-ne-300 cursor-pointer" onClick={showInstructions}>How to Play.</span></p>
-        <div className="flex flex-wrap lg:place-content-center">
-          <input
-            name="gameId"
-            type="text"
-            value={gameId}
-            onChange={e => setGameId(e.target.value)}
-            placeholder='Enter Game ID...'
-            className="transition duration-500 bg-white rounded-md me-2 text-black"/>
-          <a href={`${window.location.href}/${gameId}/create`} type="button" className="me-2 p-2 rounded-md bg-dt-500 dark:bg-dt-200 hover:opacity-80 text-white">Create Game</a>
-          <a href={`${window.location.href}/${gameId}`} type="button" className="p-2 rounded-md bg-dt-500 dark:bg-dt-200 hover:opacity-80 text-white">Join Game</a>
-        </div>
+    <div className="p-4 md:p-6 rounded-3xl shadow-md max-w-3xl mx-auto bg-ew-500/50">
+    <div className="text-left lg:text-center">
+      <h1 className="text-white text-3xl lg:text-6xl font-extrabold my-4 inline-block">Truth or Dare</h1>
+      <p className="max-w-3xl mb-4 text-white">{gamesData['truth-or-dare'].heroText} <span className="underline hover:text-ne-300 cursor-pointer" onClick={showInstructions}>How to Play.</span></p>
+      <div className="flex flex-wrap lg:place-content-center">
+        <input
+          name="gameId"
+          type="text"
+          value={gameId}
+          onChange={e => setGameId(e.target.value)}
+          placeholder='Enter Game ID...'
+          className="transition duration-500 bg-white rounded-md me-2 text-black"/>
+        <a href={`${window.location.href}/${gameId}/create`} type="button" className="me-2 p-2 rounded-md bg-dt-500 dark:bg-dt-200 hover:opacity-80 text-white">Create Game</a>
+        <a href={`${window.location.href}/${gameId}`} type="button" className="p-2 rounded-md bg-dt-500 dark:bg-dt-200 hover:opacity-80 text-white">Join Game</a>
       </div>
     </div>
     </div>
