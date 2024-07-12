a596adc23644dc33c6bb6c49555fcf1fa536905a
Izz Hafeez <izzhafeez@gmail.com>
2024-07-12 21:56:00 +0800

    Commit src/components/games/convo-starter/ConvoStarterGameCreate.tsx

--- src/components/games/convo-starter/ConvoStarterGameCreate.tsx
+++ src/components/games/convo-starter/ConvoStarterGameCreate.tsx
@@ -92,69 +92,67 @@ function ConvoStarterGameCreate({ id }: { id: string }) {
 
   // floating form in the centre
   return (
-    <div className="p-2 h-screen">
-      <div className={`p-4 md:p-6 rounded-3xl shadow-md max-w-3xl ${questions.length > 0 ? 'mt-4' : 'mt-40'} mx-auto bg-dt-500/50`}>
-        <div className="text-left lg:text-center">
-          <h1 className="text-white text-3xl lg:text-6xl font-extrabold my-4 inline-block">Convo Starter</h1>
-          <p className="max-w-3xl mb-4 text-white">Create a new Convo Starter game by filling out the form below. We will take your preferences into account and give you a custom experience.</p>
-          <div className="grid gap-2">
-            <div className="grid">
-              <label className="text-left font-bold min-w-10 text-white">Purpose:</label>
-              <textarea
-                name="purpose"
-                value={purpose}
-                onChange={e => setPurpose(e.target.value)}
-                placeholder='e.g. Social networking event for Freshmen...'
-                className="transition duration-500 bg-white text-black rounded-md"/>
-            </div>
-            <div className="grid">
-              <label className="text-left font-bold min-w-10 text-white">Participant Description:</label>
-              <textarea
-                name="information"
-                value={information}
-                onChange={e => setInformation(e.target.value)}
-                placeholder='e.g. People who like learning and meeting new people...'
-                className="transition duration-500 bg-white text-black rounded-md"/>
-            </div>
-            <div className="grid">
-              <label className="text-left font-bold min-w-10 text-white">Quantity:</label>
-              <input
-                name="quantity"
-                type="number"
-                value={quantity}
-                onChange={e => setQuantity(Number.parseInt(e.target.value))}
-                placeholder='Enter Quantity...'
-                className="transition duration-500 bg-white text-black rounded-md"/>
-            </div>
-            <button onClick={onGenerate} type="button" className="ms-auto w-40 mt-4 p-2 rounded-md bg-cc-500 dark:bg-dt-200 hover:opacity-80 text-white">Generate Cards</button>
+    <div className={`p-4 md:p-6 rounded-3xl shadow-md max-w-3xl mx-auto bg-dt-500/50`}>
+      <div className="text-left lg:text-center">
+        <h1 className="text-white text-3xl lg:text-6xl font-extrabold my-4 inline-block">Convo Starter</h1>
+        <p className="max-w-3xl mb-4 text-white">Create a new Convo Starter game by filling out the form below. We will take your preferences into account and give you a custom experience.</p>
+        <div className="grid gap-2">
+          <div className="grid">
+            <label className="text-left font-bold min-w-10 text-white">Purpose:</label>
+            <textarea
+              name="purpose"
+              value={purpose}
+              onChange={e => setPurpose(e.target.value)}
+              placeholder='e.g. Social networking event for Freshmen...'
+              className="transition duration-500 bg-white text-black rounded-md"/>
           </div>
-          {questions.length > 0 && <div className="grid gap-2">
-            <h2 className="text-white text-2xl font-extrabold my-4">Generated Cards</h2>
-            <div className="grid gap-2">
-              {questions.map((question, index) => (
-                <div key={index} className="grid">
-                  <label className="text-left font-bold min-w-10 text-white">Question {index + 1}:</label>
-                  <div className="flex">
-                    <input
-                      name={`question-${index}`}
-                      value={question}
-                      onChange={e => updateQuestions(e.target.value, index)}
-                      className="bg-white text-black rounded-l-md px-2 w-full"/>
-                    <button onClick={() => deleteQuestion(index)} type="button" className="ms-auto w-40 p-2 rounded-r-md bg-ns-500 hover:opacity-80 text-white">Delete</button>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="flex">
-              <button onClick={saveQuestions} type="button" className="w-40 mt-4 p-2 rounded-md text-dt-500 hover:opacity-80 bg-white">Save</button>
-              <button onClick={() => {
-                const new_link = window.location.href.split('/');
-                new_link.pop();
-                window.location.href = new_link.join('/');
-              }} type="button" className="ms-auto w-40 mt-4 p-2 rounded-md bg-ew-500 hover:opacity-80 text-white">Start</button>
-            </div>
-          </div>}
+          <div className="grid">
+            <label className="text-left font-bold min-w-10 text-white">Participant Description:</label>
+            <textarea
+              name="information"
+              value={information}
+              onChange={e => setInformation(e.target.value)}
+              placeholder='e.g. People who like learning and meeting new people...'
+              className="transition duration-500 bg-white text-black rounded-md"/>
+          </div>
+          <div className="grid">
+            <label className="text-left font-bold min-w-10 text-white">Quantity:</label>
+            <input
+              name="quantity"
+              type="number"
+              value={quantity}
+              onChange={e => setQuantity(Number.parseInt(e.target.value))}
+              placeholder='Enter Quantity...'
+              className="transition duration-500 bg-white text-black rounded-md"/>
+          </div>
+          <button onClick={onGenerate} type="button" className="ms-auto w-40 mt-4 p-2 rounded-md bg-cc-500 dark:bg-dt-200 hover:opacity-80 text-white">Generate Cards</button>
         </div>
+        {questions.length > 0 && <div className="grid gap-2">
+          <h2 className="text-white text-2xl font-extrabold my-4">Generated Cards</h2>
+          <div className="grid gap-2">
+            {questions.map((question, index) => (
+              <div key={index} className="grid">
+                <label className="text-left font-bold min-w-10 text-white">Question {index + 1}:</label>
+                <div className="flex">
+                  <input
+                    name={`question-${index}`}
+                    value={question}
+                    onChange={e => updateQuestions(e.target.value, index)}
+                    className="bg-white text-black rounded-l-md px-2 w-full"/>
+                  <button onClick={() => deleteQuestion(index)} type="button" className="ms-auto w-40 p-2 rounded-r-md bg-ns-500 hover:opacity-80 text-white">Delete</button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex">
+            <button onClick={saveQuestions} type="button" className="w-40 mt-4 p-2 rounded-md text-dt-500 hover:opacity-80 bg-white">Save</button>
+            <button onClick={() => {
+              const new_link = window.location.href.split('/');
+              new_link.pop();
+              window.location.href = new_link.join('/');
+            }} type="button" className="ms-auto w-40 mt-4 p-2 rounded-md bg-ew-500 hover:opacity-80 text-white">Start</button>
+          </div>
+        </div>}
       </div>
     </div>
   );
