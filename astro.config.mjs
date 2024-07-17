5d0bca81423593771a43a5e7ffbae29c56aebce6
Izz Hafeez <izzhafeez@gmail.com>
2024-07-17 19:29:48 +0800

    Commit astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,15 +8,19 @@ import spotlightjs from "@spotlightjs/astro";
 import svelte from "@astrojs/svelte";
 import netlify from "@astrojs/netlify";
 import sentry from "@sentry/astro";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: 'file',
+  },
   output: "hybrid",
   integrations: [react(), tailwind(), metaTags(), sitemap(), sentry(), spotlightjs(), vue({
     template: {
       transformAssetUrls: false
     }
-  }), svelte()],
+  }), svelte(), pagefind()],
   adapter: netlify(),
   site: "https://izzhafeez.com",
   renderers: ['@astrojs/renderer-react']
