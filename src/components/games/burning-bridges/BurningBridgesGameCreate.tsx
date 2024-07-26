5202ad62612221acb897bba064b3e44094e240cc
Izz Hafeez <izzhafeez@gmail.com>
2024-07-26 17:53:02 +0800

    Commit src/components/games/burning-bridges/BurningBridgesGameCreate.tsx

--- src/components/games/burning-bridges/BurningBridgesGameCreate.tsx
+++ src/components/games/burning-bridges/BurningBridgesGameCreate.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 
 function ConvoStarterGameCreate({ id }: { id: string }) {
