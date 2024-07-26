1666eeb9466e943c7ddacc974cfa5fea2ff49242
Izz Hafeez <izzhafeez@gmail.com>
2024-07-26 17:53:02 +0800

    Commit src/components/games/frequency-guessr/FrequencyGuessr.tsx

--- src/components/games/frequency-guessr/FrequencyGuessr.tsx
+++ src/components/games/frequency-guessr/FrequencyGuessr.tsx
@@ -257,7 +257,7 @@ function FrequencyGuessr({ id }: { id: string }) {
   return <>
     <div className="p-2 max-w-3xl mx-auto">
       {gameStatus !== 'UNJOINED' && <>
-        <h1 className="animate-linear bg-[length:200%_auto] bg-gradient-to-r from-dt-500 to-dt-300 text-transparent bg-clip-text text-6xl font-extrabold my-4 inline-block">FrequencyGuessr</h1>
+        <h1 className="animate-linear bg-[length:200%_auto] bg-gradient-to-r from-dt-500 to-dt-300 text-transparent bg-clip-text text-6xl font-extrabold my-4 inline-block">Frequency Guessr</h1>
         </>}
       {gameStatus === 'JOINED' && <>
         <div className="flex gap-2 mx-auto my-4">
@@ -268,7 +268,7 @@ function FrequencyGuessr({ id }: { id: string }) {
         </div>
         </>}
       {gameStatus === 'UNJOINED' && <div className="mt-40 text-center">
-        <h1 className="animate-linear bg-[length:200%_auto] bg-gradient-to-r from-dt-500 to-dt-300 text-transparent bg-clip-text text-6xl font-extrabold my-4 inline-block">FrequencyGuessr</h1>
+        <h1 className="animate-linear bg-[length:200%_auto] bg-gradient-to-r from-dt-500 to-dt-300 text-transparent bg-clip-text text-6xl font-extrabold my-4 inline-block">Frequency Guessr</h1>
         <p className="max-w-3xl mx-auto mb-4 ">{instructions}</p>
         <input
           name="name"
