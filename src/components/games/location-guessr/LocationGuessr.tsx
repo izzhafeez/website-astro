62921eef5d49a41ac9b567bd9361798aea0c21d7
Izz Hafeez <izzhafeez@gmail.com>
2024-07-28 21:43:20 +0800

    Commit src/components/games/location-guessr/LocationGuessr.tsx

--- src/components/games/location-guessr/LocationGuessr.tsx
+++ src/components/games/location-guessr/LocationGuessr.tsx
@@ -3,16 +3,11 @@ import useWebSocket from 'react-use-websocket';
 import gamesData from '../../../data/games/games.json';
 import Swal from 'sweetalert2';
 import { CircleMarker, MapContainer, TileLayer, Tooltip, useMapEvents } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'
-
-const copySvg = <svg className="invert dark:invert-0" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path fillRule="evenodd" clipRule="evenodd" d="M21 8C21 6.34315 19.6569 5 18 5H10C8.34315 5 7 6.34315 7 8V20C7 21.6569 8.34315 23 10 23H18C19.6569 23 21 21.6569 21 20V8ZM19 8C19 7.44772 18.5523 7 18 7H10C9.44772 7 9 7.44772 9 8V20C9 20.5523 9.44772 21 10 21H18C18.5523 21 19 20.5523 19 20V8Z" fill="#0F0F0F"/>
-  <path d="M6 3H16C16.5523 3 17 2.55228 17 2C17 1.44772 16.5523 1 16 1H6C4.34315 1 3 2.34315 3 4V18C3 18.5523 3.44772 19 4 19C4.55228 19 5 18.5523 5 18V4C5 3.44772 5.44772 3 6 3Z" fill="#0F0F0F"/>
-</svg>;
-
-const startSvg = <svg className="invert" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M16.6582 9.28638C18.098 10.1862 18.8178 10.6361 19.0647 11.2122C19.2803 11.7152 19.2803 12.2847 19.0647 12.7878C18.8178 13.3638 18.098 13.8137 16.6582 14.7136L9.896 18.94C8.29805 19.9387 7.49907 20.4381 6.83973 20.385C6.26501 20.3388 5.73818 20.0469 5.3944 19.584C5 19.053 5 18.1108 5 16.2264V7.77357C5 5.88919 5 4.94701 5.3944 4.41598C5.73818 3.9531 6.26501 3.66111 6.83973 3.6149C7.49907 3.5619 8.29805 4.06126 9.896 5.05998L16.6582 9.28638Z" stroke="#000000" strokeWidth="2" strokeLinejoin="round"/>
-</svg>;
+import 'leaflet/dist/leaflet.css';
+import lifecycle from '../common/GameLifecycle';
+import GameStart from '../common/GameStart';
+import GameTitle from '../common/GameTitle';
+import GameJoin from '../common/GameJoin';
 
 const instructions = gamesData['location-guessr'].heroText;
 
@@ -68,29 +63,11 @@ function LocationGuessr({ id, data, title }: { id: string, data: {name: string, 
     if (message.distance) setDistance(_ => message.distance);
 
     if (method === 'connect') {
-      Swal.fire({
-        icon: 'info',
-        title: `Connecting...`,
-        timer: 1500,
-        didOpen: () => {
-          Swal.showLoading();
-        }
-      });
-      setGameStatus('UNJOINED');
+      lifecycle.handleConnect(setGameStatus);
     } else if (method === 'join') {
       setGameStatus('JOINED');
     } else if (method === 'leave') {
-      if (message.name === name) {
-        Swal.fire({
-          icon: 'info',
-          title: `Leaving...`,
-          timer: 1500,
-          didOpen: () => {
-            Swal.showLoading();
-          }
-        });
-        setGameStatus('UNJOINED');
-      }
+      lifecycle.handleLeave(message.name === name, setGameStatus);
     } else if (method === 'start') {
       setGameStatus('PLAYING');
     } else if (method === 'next') {
@@ -123,36 +100,13 @@ function LocationGuessr({ id, data, title }: { id: string, data: {name: string, 
         </table>`
       })
     } else if (method === 'end') {
-      Swal.fire({
-        icon: 'info',
-        title: `Game Over! ${message.winner} Wins!`,
-        text: `The game will end in 5 seconds...`,
-        timer: 5000,
-      })
-      setGameStatus('JOINED');
+      lifecycle.handleEndGame(message.winner, setGameStatus);
     } else if (method === 'connect_error') {
-      Swal.fire({
-        icon: 'info',
-        title: `Game not found!`,
-        text: `Creating a new game at ${id}...`,
-        timerProgressBar: true,
-        timer: 2000
-      });
-      setGameStatus('UNJOINED');
+      lifecycle.handleConnectError(id, setGameStatus);
     } else if (method === 'join_error') {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: message.message
-      });
-      setGameStatus('UNJOINED');
+      lifecycle.handleJoinError(message.message, setGameStatus);
     } else if (method === 'start_error') {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: message.message
-      });
-      setGameStatus('JOINED');
+      lifecycle.handleStartError(message.message, setGameStatus);
     }
   }, [lastMessage]);
 
@@ -169,40 +123,6 @@ function LocationGuessr({ id, data, title }: { id: string, data: {name: string, 
   const startGame = () => {
     sendJsonMessage({ method: 'start', deck_size: 100 });
     setGameStatus('PLAYING')
-  }
-
-  const copyGameLink = () => {
-    navigator.clipboard.writeText(window.location.href);
-    const toast = Swal.mixin({
-      toast: true,
-      position: "top-end",
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.onmouseenter = Swal.stopTimer;
-        toast.onmouseleave = Swal.resumeTimer;
-      }
-    });
-    toast.fire({
-      icon: "success",
-      title: "Link Copied!"
-    });
-  }
-
-  const promptLeave = () => {
-    Swal.fire({
-      icon: 'warning',
-      title: 'Are you sure?',
-      text: 'You will be removed from the game!',
-      showCancelButton: true,
-      confirmButtonText: 'Yes, leave!',
-      cancelButtonText: 'No, stay!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        leaveGame();
-      }
-    });
   }
 
   const submitGuess = () => {
@@ -237,13 +157,7 @@ function LocationGuessr({ id, data, title }: { id: string, data: {name: string, 
     })
   };
 
-  const showHowToPlay = () => {
-    Swal.fire({
-      icon: 'info',
-      title: 'How to Play',
-      text: `Based on the given color, guess the hex code of the color. The the number of points you get is based on how close you are to the actual color. The player with the most points at the end of the game wins!`,
-    });
-  }
+  const howToPlay = `Based on the given name, guess the location on the map. The player with the SECOND closest guess gets a point!`;
 
   const LocationFinderDummy = () => {
     const map = useMapEvents({
@@ -259,30 +173,9 @@ function LocationGuessr({ id, data, title }: { id: string, data: {name: string, 
 
   return <>
     <div className="p-2 max-w-3xl mx-auto">
-      {gameStatus !== 'UNJOINED' && <>
-        <h1 className="animate-linear bg-[length:200%_auto] bg-gradient-to-r from-dt-500 to-dt-300 text-transparent bg-clip-text text-6xl font-extrabold my-4 inline-block">Location Guessr</h1>
-        </>}
-      {gameStatus === 'JOINED' && <>
-        <p className="max-w-3xl mb-4 ">{instructions} Deck: <span className="text-dt-500 dark:text-dt-300 font-bold">{title}</span></p>
-        <div className="flex gap-2 mx-auto my-4">
-          {/* start */}
-          <button onClick={startGame} className="p-2 rounded-md bg-ew-500 hover:opacity-80 text-white flex gap-1"><span className="my-auto">{startSvg}</span> Start Game</button>
-          {/* copy link */}
-          <button onClick={copyGameLink} className="p-2 rounded-md bg-dt-500 dark:bg-dt-300 dark:text-black hover:opacity-80 text-white flex gap-1"><span className="my-auto">{copySvg}</span> Copy Link</button>
-        </div>
-        </>}
-      {gameStatus === 'UNJOINED' && <div className="mt-40 text-center">
-        <h1 className="animate-linear bg-[length:200%_auto] bg-gradient-to-r from-dt-500 to-dt-300 text-transparent bg-clip-text text-6xl font-extrabold my-4 inline-block">Location Guessr</h1>
-        <p className="max-w-3xl mx-auto mb-4 ">{instructions}</p>
-        <input
-          name="name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder='Enter your name...'
-          className="transition duration-500 bg-white dark:bg-gray-700 rounded-md me-2"/>
-        <button onClick={joinGame} className="p-2 rounded-md bg-dt-500 dark:bg-dt-300 dark:text-black hover:opacity-80 text-white">Join Game</button>
-      </div>}
+      {gameStatus !== 'UNJOINED' && <GameTitle title={title}/>}
+      {gameStatus === 'JOINED' && <GameStart instructions={instructions} startGame={startGame}/>}
+      {gameStatus === 'UNJOINED' && <GameJoin title={title} instructions={instructions} name={name} setName={setName} joinGame={joinGame}/>}
 
       {/* show all players */}
       {gameStatus !== 'UNJOINED' && <>
@@ -293,8 +186,8 @@ function LocationGuessr({ id, data, title }: { id: string, data: {name: string, 
             <span className="text-white bg-dt-500 dark:bg-dt-300 dark:text-black rounded-md p-1 me-1">{playerName}{playerName === name && ' (you)'}{playerData.guess && gameStatus !== 'EVALUATING' && ' (played)'}{playerData.acknowledged && ' (ready)'}</span> {playerData.points} Points
           </li>
         ))}
-        <li><span onClick={showHowToPlay} className="bg-cc-500 text-white rounded-md p-1 hover:opacity-50 cursor-pointer">How to Play</span></li>
-        <li><span onClick={promptLeave} className="bg-ns-500 text-white rounded-md p-1 hover:opacity-50 cursor-pointer">Leave Game</span></li>
+        <li><span onClick={() => lifecycle.showHowToPlay(howToPlay)} className="bg-cc-500 text-white rounded-md p-1 hover:opacity-50 cursor-pointer">How to Play</span></li>
+        <li><span onClick={() => lifecycle.promptLeave(leaveGame)} className="bg-ns-500 text-white rounded-md p-1 hover:opacity-50 cursor-pointer">Leave Game</span></li>
       </ul>
       </>}
 
