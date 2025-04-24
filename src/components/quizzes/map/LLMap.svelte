a2b95081957bf5a3f58d7db9e7ebccad3da8a3f9
Izz Hafeez <izzhafeez@gmail.com>
2025-04-25 00:55:24 +0800

    Commit src/components/quizzes/map/LLMap.svelte

--- src/components/quizzes/map/LLMap.svelte
+++ src/components/quizzes/map/LLMap.svelte
@@ -13,7 +13,8 @@ const tileOptions = {
   hot: { url: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' },
   arcgis: { url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' },
   white: { url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
-	        &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>` }
+	        &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>` },
+  marble: { url: "https://maps.gnosis.earth/ogcapi/collections/blueMarble/map/tiles/WebMercatorQuad" }
 }
 
 const svg = (width, height, fill) => `<svg width="${width}px" height="${height}px" viewBox="-4 0 32 32" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
@@ -32,7 +33,7 @@ function createMap(container) {
     fullscreen: true
   }).setView([1.35, 103.85], 13);
   L.tileLayer(
-    tileOptions.white.url,
+    tileOptions.arcgis.url,
     {
       attribution: tileOptions.osm.attribution,
       maxNativeZoom: 17,
@@ -71,7 +72,7 @@ function createFeatures() {
     if (location === null) continue;
     const feature = createMarker(location);
     featureList.push(feature);
-    if (location.label.match(defaultRegex) || location.searchTerms.match(defaultRegex)) {
+    if (location.label.match(defaultRegex)) {
       featureGroup.addLayer(feature);
       featureCount += 1;
     }
@@ -93,38 +94,30 @@ function mapAction(container) {
 }
 
 toRemove.subscribe(value => {
-  if (value === null) return;
+  if (value === null || !map) return;
   const feature = featureList[value];
-  if (!!map) {
-    map.removeLayer(feature);
-  }
+  map.removeLayer(feature);
 })
 
 toAdd.subscribe(value => {
-  if (value == null) return;
+  if (value == null || !map) return;
   const feature = featureList[value];
-  if (!!map) {
-    map.addLayer(feature);
-  }
+  map.addLayer(feature);
 })
 
 toHideTooltip.subscribe(value => {
-  if (value == null) return;
+  if (value == null || !map) return;
   const feature = featureList[value];
-  if (!!map) {
-    feature.unbindTooltip();
-  }
+  feature.unbindTooltip();
 })
 
 toShowTooltip.subscribe(value => {
-  if (value == null) return;
+  if (value == null || !map) return;
   const feature = featureList[value];
-  if (!!map) {
-    const tooltip = tooltips[value];
-    feature.bindTooltip(tooltip, {
-      permanent: false
-    });
-  }
+  const tooltip = tooltips[value];
+  feature.bindTooltip(tooltip, {
+    permanent: false
+  });
 })
 
 </script>
