e09c7a8e1e8c991c46c4325b6717d782d7d15057
Izz Hafeez <izzhafeez@gmail.com>
2025-04-25 00:55:23 +0800

    Commit src/components/quizzes/color/ColorQuiz.svelte

--- src/components/quizzes/color/ColorQuiz.svelte
+++ src/components/quizzes/color/ColorQuiz.svelte
@@ -4,41 +4,35 @@
   import seededRandom from "../../common/seededRandom";
   import DailyChoice from "../DailyChoice.svelte";
 
-  export let key;
-  export let isDaily = false;
-
-  let date;
+  export let params;
   
   let isStart = false;
-  let name;
-  let randomiserSeed = Math.round(Math.random() * 1000000);
-  let randomiser = seededRandom(randomiserSeed);
-  let randomiseSeed = () => {
-    randomiserSeed = Math.round(Math.random() * 1000000);
-    randomiser = seededRandom(randomiserSeed);
-  }
-  let decodeSeed = () => {
-    if (isNaN(randomiserSeed)) {
-      randomiserSeed = 0;
-    }
-    randomiser = seededRandom(randomiserSeed);
-  }
+  let seed = parseInt(params.seed) || Math.floor(Math.random() * 100000000);
+
+  let randomiser;
 
   let handleStart = () => {
+    randomiser = seededRandom(seed);
+    randomiser();
     isStart = true;
   }
+
+  const randomiseSeed = () => {
+    seed = Math.floor(Math.random() * 100000000);
+  }
+
+  const setTodaySeed = () => {
+    // ddmmyyyy
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const yyyy = today.getFullYear();
+    seed = yyyy + mm + dd;
+  };
 </script>
 
 {#if !isStart}
-  <div class="max-w-3xl mx-auto my-8">
-    <h1 class="text-5xl font-black animate-text bg-gradient-to-r from-ns-500 via-ns-400 to-ns-300 bg-clip-text text-transparent">COLOR GUESSR</h1>
-    <p class="my-2">Given a color, guess the hexcode in RGB format. For example, pure red would be #FF0000. Your score is based on how close your guess is to the actual hexcode. You have 5 rounds. Good luck!</p>
-    {#if !isDaily}
-      <StartPage {key} bind:name={name} bind:isStart={isStart} bind:randomiserSeed={randomiserSeed} {decodeSeed} {randomiseSeed}/>
-    {:else}
-      <DailyChoice {key} name="COLOR QUIZ" bind:date={date} bind:isStart={isStart} bind:randomiserSeed={randomiserSeed} bind:randomiser={randomiser} {handleStart} fullKey={`color`}/>
-    {/if}
-  </div>
+  <StartPage bind:isStart={isStart} bind:seed={seed} {randomiseSeed} {setTodaySeed} {handleStart}/>
 {:else}
-  <GamePage {key} bind:name={name} bind:isStart={isStart} {randomiser} {decodeSeed} {randomiserSeed} {date}/>
+  <GamePage bind:isStart={isStart} {handleStart} {randomiser} {seed} {randomiseSeed}/>
 {/if}
