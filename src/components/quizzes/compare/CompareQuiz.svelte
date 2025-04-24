7714a5c94edde1c62feb52611e072f695a71fe5a
Izz Hafeez <izzhafeez@gmail.com>
2025-04-25 00:55:23 +0800

    Commit src/components/quizzes/compare/CompareQuiz.svelte

--- src/components/quizzes/compare/CompareQuiz.svelte
+++ src/components/quizzes/compare/CompareQuiz.svelte
@@ -3,49 +3,34 @@ import StartPage from "./StartPage.svelte";
 import GamePage from "./GamePage.svelte";
 import seededRandom from "../../common/seededRandom";
 
-export let key;
 export let title;
 export let data;
-export let instructions;
-export let difficulty = 2;
+export let params;
+let seed = parseInt(params.seed) || Math.floor(Math.random() * 100000000);
 
-const difficultyMappings = {
-  1: 5,
-  2: 15,
-  3: 50,
-  4: 150,
-  5: 10000
-}
-
-let name;
 let isPlaying = false;
-let fields = Object.keys(Object.values(data)[0]);
+let fields = Object.keys(Object.values(data)[0]).filter(k => typeof Object.values(data)[0][k] == "number");
 let field = fields[0];
 
-let randomiserSeed = Math.floor(Math.random() * 1000000);
-let randomiser = seededRandom(randomiserSeed);
-let encodeSeed = () => {
-  let fieldIndex = fields.indexOf(field);
-  return `${difficulty}${fieldIndex}${randomiserSeed}`;
+let randomiser;
+
+const randomiseSeed = () => {
+  seed = Math.floor(Math.random() * 100000000);
 }
-let seed = encodeSeed();
-let randomiseSeed = () => {
-  randomiserSeed = Math.floor(Math.random() * 1000000);
-  randomiser = seededRandom(randomiserSeed);
-  seed = encodeSeed();
-}
-let decodeSeed = () => {
-  field = fields[parseInt(seed[1])];
-  difficulty = parseInt(seed[0]);
-  randomiserSeed = parseInt(seed.slice(2));
-  randomiser = seededRandom(randomiserSeed);
-}
+
+const setTodaySeed = () => {
+  // ddmmyyyy
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+  const yyyy = today.getFullYear();
+  seed = yyyy + mm + dd;
+  field = fields[0];
+};
 </script>
 
-<div class="">
 {#if !isPlaying}
-<StartPage {title} {instructions} {key} bind:name={name} bind:isPlaying={isPlaying} bind:difficulty={difficulty} bind:field={field} {fields} bind:seed={seed} {randomiseSeed} {decodeSeed}/>
+<StartPage {title} bind:randomiser={randomiser} {randomiseSeed} {setTodaySeed} bind:isPlaying={isPlaying} bind:field={field} {fields} bind:seed={seed}/>
 {:else}
-<GamePage {key} {data} {name} count={difficultyMappings[difficulty]} {field} {seed} {randomiser} {difficulty}/>
+<GamePage {data} {field} {seed} {randomiser} {randomiseSeed} {title}/>
 {/if}
-</div>
