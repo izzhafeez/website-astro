b1fcf1ab044e5d6b99799dfabf357b76121cddb4
Izz Hafeez <izzhafeez@gmail.com>
2025-04-25 00:55:23 +0800

    Commit src/components/quizzes/blurry/BlurryQuiz.svelte

--- src/components/quizzes/blurry/BlurryQuiz.svelte
+++ src/components/quizzes/blurry/BlurryQuiz.svelte
@@ -8,74 +8,54 @@
     export let title: string;
     export let instructions: string;
     export let names: string[];
-    export let key: string;
-    export let isDaily = false;
+    export let params: any;
 
-    let date;
+    let seed: string = parseInt(params.seed) || Math.floor(Math.random() * 100000000);
+    let randomiser = seededRandom(seed);
+    let blurValue = params.blur || "MD";
+    let fontSize = parseInt(params.font) || 80;
 
-    let name = "";
     let isStart = false;
     let possible_blurValues = ["XS", "SM", "MD", "LG", "XL", "2XL", "3XL"];
     let possible_fontSizes = [20, 30, 40, 60, 80, 120, 160];
-    let blurValue = "MD";
-    let fontSize = 60;
-    let randomiserSeed = Math.round(Math.random() * 1000000);
-    let randomiser = seededRandom(randomiserSeed);
-    let seed = encodeSeed(randomiserSeed);
     let toGuess;
 
-    function decodeSeed() {
-        if (isNaN(seed)) {
-            return;
-        }
-        const fontSizeIndex = parseInt(seed[0]);
-        fontSize = possible_fontSizes[fontSizeIndex];
-        const blurValueIndex = parseInt(seed[1]);
-        blurValue = possible_blurValues[blurValueIndex];
-        randomiserSeed = parseInt(seed.slice(2));
-        randomiser = seededRandom(randomiserSeed);
+    let randomiseSeed = () => {
+        seed = Math.floor(Math.random() * 100000000);
+        randomiser = seededRandom(seed);
     }
 
-    function encodeSeed(randomiserSeed: string) {
-        const fontSizeIndex = possible_fontSizes.indexOf(fontSize);
-        const blurValueIndex = possible_blurValues.indexOf(blurValue);
-        return `${fontSizeIndex}${blurValueIndex}${randomiserSeed}`;
+    let setTodaySeed = () => {
+        // ddmmyyyy
+        const today = new Date();
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+        const yyyy = today.getFullYear();
+        seed = yyyy + mm + dd;
+        blurValue = "MD";
+        fontSize = 60;
     }
-
-    const randomiseSeed = () => {
-        randomiserSeed = Math.round(Math.random() * 1000000);
-        randomiser = seededRandom(randomiserSeed);
-        seed = encodeSeed(randomiserSeed);
-    };
 
     function handleStart() {
-        randomiser = seededRandom(randomiserSeed);
+        if (isNaN(seed)) seed = Math.floor(Math.random() * 100000000);
+        if (possible_blurValues.indexOf(blurValue) == -1) blurValue = "MD";
+        if (isNaN(fontSize)) fontSize = 80;
+        // find closest font size
+        if (possible_fontSizes.indexOf(fontSize) == -1) {
+            let closest = possible_fontSizes.reduce((prev, curr) => Math.abs(curr - fontSize) < Math.abs(prev - fontSize) ? curr : prev);
+            fontSize = closest;
+        }
+
+        randomiser = seededRandom(seed);
         randomiser();
         toGuess = names[Math.floor(randomiser() * names.length)];
         isStart = true;
-        if (isNaN(seed) || seed.length < 3 || parseInt(seed[0]) >= possible_fontSizes.length || parseInt(seed[1]) >= possible_blurValues.length) {
-            Swal.fire({
-                title: "Invalid Seed",
-                text: "Seed is invalid! Please enter a valid seed.",
-                icon: "error",
-                confirmButtonText: "OK",
-            });
-            return;
-        }
     }
 </script>
 
 {#if isStart}
-    <GamePage bind:randomiser={randomiser} {seed} {randomiserSeed} bind:isStart={isStart} {blurValue} {fontSize} bind:toGuess={toGuess} {encodeSeed} {names} {name} {key} {date} {title} {isDaily}/>
+    <GamePage bind:randomiser={randomiser} {seed} bind:isStart={isStart} {blurValue} {fontSize} bind:toGuess={toGuess} {names} {title} {randomiseSeed}/>
 {:else}
-    <div class="max-w-3xl mx-auto my-8">
-        <h1 class="text-5xl font-black animate-text bg-gradient-to-r from-ns-500 via-ns-400 to-ns-300 bg-clip-text text-transparent">{title.toUpperCase()}</h1>
-        <p class="my-4 max-w-3xl">{instructions}</p>
-        {#if isDaily}
-            <DailyChoice bind:randomiserSeed={randomiserSeed} handleStart={handleStart} bind:randomiser={randomiser} name={`BLURRYGUESSR: ${title}`} bind:date={date} fullKey={`blurry-${key}`}/>
-        {:else}
-            <StartPage bind:blurValue={blurValue} bind:fontSize={fontSize} {handleStart} {title} {instructions} bind:seed={seed} {randomiseSeed} {decodeSeed} bind:name={name} {key}/>
-        {/if}
-    </div>
+    <StartPage bind:blurValue={blurValue} bind:fontSize={fontSize} {handleStart} {title} {instructions} bind:seed={seed} {randomiseSeed} {setTodaySeed}/>
 {/if}
 
