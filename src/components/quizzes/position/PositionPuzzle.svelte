41212806f3cdb7bc3f7ad491fff9197ae43d1cfa
Izz Hafeez <izzhafeez@gmail.com>
2025-04-25 00:55:24 +0800

    Commit src/components/quizzes/position/PositionPuzzle.svelte

--- src/components/quizzes/position/PositionPuzzle.svelte
+++ src/components/quizzes/position/PositionPuzzle.svelte
@@ -4,44 +4,48 @@
   import seededRandom, {shuffle} from '../../common/seededRandom';
   import Swal from 'sweetalert2';
   import DailyChoice from '../DailyChoice.svelte';
+  import { standardiseWithoutParen } from '../../../data/standardiseName';
 
   export let title;
   export let data;
-  export let instructions;
+  export let params;
   export let key;
-  export let isDaily = false;
 
-  let date;
+  let seed = parseInt(params.seed) || Math.floor(Math.random() * 100000000);
+  let randomiser = seededRandom(seed);
+  let N = parseInt(params.N) || 8;
 
-  let N = 8;
   let possible_N = [3, 4, 6, 8, 10, 12];
   let isRotate = true;
   let locations = [];
   let positions = [];
 
   let isStart = false;
-  let randomiserSeed = Math.floor(Math.random() * 1000000);
-  let randomiser = seededRandom(randomiserSeed);
-  let encodeSeed = () => {
-    return `${possible_N.indexOf(N)}${isRotate ? 1 : 0}${randomiserSeed}`;
-  }
+
   let randomiseSeed = () => {
-    randomiserSeed = Math.floor(Math.random() * 1000000);
-    randomiser = seededRandom(randomiserSeed);
-    seed = encodeSeed();
+    seed = Math.floor(Math.random() * 100000000);
   }
-  let seed = encodeSeed();
-  let decodeSeed = () => {
-    N = possible_N[parseInt(seed[0])];
-    isRotate = parseInt(seed[1]) === 1;
-    randomiserSeed = parseInt(seed.slice(2));
-    randomiser = seededRandom(randomiserSeed);
+
+  let setTodaySeed = () => {
+    // ddmmyyyy
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const yyyy = today.getFullYear();
+    seed = yyyy + mm + dd;
+    N = 8;
   }
 
   function handleNext() {
-    randomiser = seededRandom(randomiserSeed);
+    if (isNaN(seed)) seed = Math.floor(Math.random() * 100000000);
+    if (isNaN(N)) N = 8;
+    if (N < 3) N = 3;
+    if (N > data.length) N = data.length;
+
+    randomiser = seededRandom(seed);
+    randomiser();
     // select N random locations from the data
-    const allPositions = data.map((d) => [d.lat, d.lng]);
+    const allPositions = data.map((d) => [d[1][1], d[1][0]]);
     const maxLat = Math.max(...allPositions.map(([lat, lng]) => lat));
     const minLat = Math.min(...allPositions.map(([lat, lng]) => lat));
     const maxLng = Math.max(...allPositions.map(([lat, lng]) => lng));
@@ -52,6 +56,7 @@
     const threshold = 0.01 * maxDiff;
     let chosen = shuffle([...data], randomiser).slice(0, 2*N);
     let newChosen = [];
+
     for (let i = 0; i < chosen.length; i++) {
       let isClose = false;
       for (let j = 0; j < newChosen.length; j++) {
@@ -68,8 +73,8 @@
       }
     }
     chosen = newChosen;
-    locations = chosen.map((d) => d.name);
-    positions = chosen.map((d) => [d.lat, d.lng]);
+    locations = chosen.map((d) => standardiseWithoutParen(d[0]));
+    positions = chosen.map((d) => [d[1][1], d[1][0]]);
     if (isRotate) {
       positions = rotate(positions);
     }
@@ -117,39 +122,10 @@
 
     return adjustedCoords.map(([lat, lng]) => [lat - midLat + 0.5, lng - midLng + 0.5]);
   }
-
-  const toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 1000,
-    });
-
-  const copySeed = () => {
-      navigator.clipboard.writeText(seed);
-      toast.fire({
-          icon: 'success',
-          text: 'Copied Seed',
-      });
-  }
 </script>
 
-<div class="max-w-3xl mx-auto p-2 my-8">
-  <h1 class="text-5xl font-black animate-text bg-gradient-to-r from-ns-500 via-ns-400 to-ns-300 bg-clip-text text-transparent">{title.toUpperCase()}</h1>
-  <p class=" my-4">{instructions}
-    {#if !isDaily}
-      <button on:click={copySeed} class="underline hover:opacity-50">Copy the seed</button> and share with your friends!
-    {:else if date}
-      Daily Challenge for {date}.
-    {/if}
-  </p>
-  {#if !isStart}
-    {#if isDaily}
-      <DailyChoice bind:randomiserSeed={randomiserSeed} handleStart={handleNext} bind:randomiser={randomiser} name={`POSITION PUZZLE: ${title}`} bind:date={date} fullKey={`position-${key}`}/>
-    {:else}
-      <StartPage bind:N={N} bind:isRotate={isRotate} handleNext={handleNext} bind:isStart={isStart} {decodeSeed} {randomiseSeed} bind:seed={seed} {key}/>
-    {/if}
-  {:else}
-    <GamePage {positions} {locations} {randomiser} bind:isStart={isStart} {key} {date} {title}/>
-  {/if}
-</div>
+{#if !isStart}
+  <StartPage bind:N={N} bind:isRotate={isRotate} handleNext={handleNext} bind:isStart={isStart} {randomiseSeed} {setTodaySeed} bind:seed={seed} {key}/>
+{:else}
+  <GamePage {positions} {locations} {randomiser} bind:isStart={isStart} {title} {seed} {randomiseSeed}/>
+{/if}
