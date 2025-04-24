9b0ef160c411b2d5c560c5733b5ed607cdfc56b1
Izz Hafeez <izzhafeez@gmail.com>
2025-04-25 00:55:22 +0800

    Commit src/components/games/common/RoomCreate.tsx

--- src/components/games/common/RoomCreate.tsx
+++ src/components/games/common/RoomCreate.tsx
@@ -18,7 +18,7 @@ const RoomCreate = () => {
         value={gameId}
         onChange={e => setGameId(e.target.value)}
         placeholder='Enter Game ID...'
-        className="transition duration-500 bg-white dark:bg-gray-700 rounded-md me-2"/>
+        className="transition duration-500 bg-gray-100 dark:bg-gray-700 rounded-md me-2"/>
     </form>
     <a href={`${window.location.href}/${gameId}`} type="button" className="me-2 p-2 rounded-md bg-dt-500 dark:bg-dt-200 dark:text-black hover:opacity-80 text-white">Create Game</a>
     <a href={`${window.location.href}/${gameId}`} type="button" className="p-2 rounded-md bg-dt-500 dark:bg-dt-200 dark:text-black hover:opacity-80 text-white">Join Game</a>
