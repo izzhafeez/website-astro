251e30fc51b04a1e1ef117f7a4edb65a30082fcc
Izz Hafeez <izzhafeez@gmail.com>
2025-04-25 00:55:30 +0800

    Commit src/components/quizzes/MasterQuiz.svelte

---
+++ src/components/quizzes/MasterQuiz.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import loadData from "../../data/loadData";
+    import PositionPuzzle from "./position/PositionPuzzle.svelte";
+    import BlurryQuiz from "./blurry/BlurryQuiz.svelte";
+    import MapQuiz from "./map/MapQuiz.svelte";
+    import RegexWarrior from "./regex/RegexWarrior.svelte";
+    import JumbleQuiz from "./jumble/JumbleQuiz.svelte";
+    import RankQuiz from "./rank/RankQuiz.svelte";
+    import CompareQuiz from "./compare/CompareQuiz.svelte";
+    import ColorGuessr from "./color/ColorQuiz.svelte";
+    import PiePuzzle from "./pie/PiePuzzle.svelte";
+    import ConnectQuiz from "./connect/ConnectQuiz.svelte";
+    import convertSlug from "../../data/convert/convertSlug";
+    import quizData from "../../data/quizzes/quizzes.json";
+
+    export let quiz: string;
+    export let params: any
+
+    let title = convertSlug(quiz);
+    let data: any;
+    const splitted = quiz.split("-");
+    const quizType = splitted[0];
+    let instructions = quizData[quizType].heroText;
+
+    onMount(async () => {
+        data = await loadData(quiz);
+    });
+</script>
+
+{#if data}
+    {#if quizType == "position"}
+        <PositionPuzzle {data} {title} {instructions} {params}/>
+    {:else if quizType == "blurry"}
+        <BlurryQuiz names={data} {title} {instructions} {params}/>
+    {:else if quizType == "map"}
+        <MapQuiz {data} {title} {instructions} {params}/>
+    {:else if quizType == "regex"}
+        <RegexWarrior names={data} {title} {instructions} {params}/>
+    {:else if quizType == "jumble"}
+        <JumbleQuiz names={data} {title} {instructions} {params}/>
+    {:else if quizType == "rank"}
+        <RankQuiz {data} {title} {instructions} {params}/>
+    {:else if quizType == "compare"}
+        <CompareQuiz {data} {title} {instructions} {params}/>
+    {:else if quizType == "color"}
+        <ColorGuessr {title} {instructions} {params}/>
+    {:else if quizType == "pie"}
+        <PiePuzzle data={Object.entries(data)} {title} {instructions} {params}/>
+    {:else if quizType == "connect"}
+        <ConnectQuiz {title} {data} {instructions} {params}/>
+    {:else}
+        <p>Quiz type not found</p>
+    {/if}
+{:else}
+    <p>Loading...</p>
+{/if}
