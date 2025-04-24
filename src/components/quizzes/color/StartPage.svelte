950564750ed4ae3c6da8c853a85dde7e121b0827
Izz Hafeez <izzhafeez@gmail.com>
2025-04-25 00:55:23 +0800

    Commit src/components/quizzes/color/StartPage.svelte

--- src/components/quizzes/color/StartPage.svelte
+++ src/components/quizzes/color/StartPage.svelte
@@ -2,34 +2,26 @@
   import Leaderboard from "../Leaderboard.svelte";
   import { fly, fade } from 'svelte/transition';
 
-  export let key: string;
-  export let name: string;
   export let isStart: boolean;
-  export let randomiserSeed: string;
+  export let seed: string;
   export let randomiseSeed: () => void;
-  export let decodeSeed: () => void;
+  export let setTodaySeed: () => void;
+  export let handleStart: () => void;
 
   const title = "Color Quiz";
   const instructions = "Given a color, guess the hexcode in RGB format. For example, pure red would be #FF0000. Your score is based on how close your guess is to the actual hexcode. You have 5 rounds. Good luck!";
-
-  function handleStart() {
-    isStart = true;
-  }
 </script>
 
 <div in:fade={{}}>
   <label for="seed" class="">Seed: </label>
   <div class="flex gap-2">
-    <input type="text" id="seed" bind:value={randomiserSeed} on:change={decodeSeed} on:keyup={decodeSeed} class="dark:bg-gray-700 rounded-md px-2 py-1 my-2" />
-    <button on:click={randomiseSeed} class='bg-ew-500 hover:bg-ew-300 text-white rounded-lg py-2 px-4 my-2'>Randomise</button>
+    <input type="text" id="seed" bind:value={seed} class="dark:bg-gray-700 rounded-md px-2 py-1 my-2" />
+    <button on:click={randomiseSeed} class='bg-ew-300/30 py-1 px-2 rounded-md text-ew-700 dark:text-ew-300 hover:bg-ew-300/50 border-2 border-ew-500/50 my-auto'>Randomise</button>
+    <button on:click={setTodaySeed} class='bg-cc-300/20 py-1 px-2 rounded-md text-cc-700 dark:text-cc-300 hover:bg-cc-300/50 border-2 border-cc-500/50 my-auto'>Today's Challenge</button>
   </div>
-  <label for="regex" class="">Your Name (Optional):</label>
-  <input name="regex" bind:value={name} class='transition duration-500 bg-white dark:bg-gray-700 my-2 border-gray-500/50 border-[1px] rounded-md p-1' placeholder=""/>
   <div class="flex py-2 gap-2">
-    <Leaderboard type="color" key="" params={randomiserSeed}/>
-    <a href={`/quizzes/color/daily-challenge`} class="bg-cc-300/20 py-1 px-2 rounded-md text-cc-500 dark:text-cc-300 hover:bg-cc-300/50">Daily Challenge</a>
     <button
-      class="bg-ew-300/20 py-1 px-2 rounded-md text-ew-500 dark:text-ew-300 hover:bg-ew-300/50"
+      class="bg-ew-300/30 py-1 px-2 rounded-md text-ew-700 dark:text-ew-300 hover:bg-ew-300/50 border-2 border-ew-500/50"
       on:click={handleStart}
     >
       Start Quiz
