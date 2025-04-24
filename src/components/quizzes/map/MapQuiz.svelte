a28200bef10c80806b2b177971b593972ac28f50
Izz Hafeez <izzhafeez@gmail.com>
2025-04-25 00:55:24 +0800

    Commit src/components/quizzes/map/MapQuiz.svelte

--- src/components/quizzes/map/MapQuiz.svelte
+++ src/components/quizzes/map/MapQuiz.svelte
@@ -1,16 +1,56 @@
 <script>
+  import { onMount } from "svelte";
   import LLMap from "./LLMap.svelte";
   import InputBox from "./InputBox.svelte";
-  export let locationDict;
-  export let locationList;
-  export let defaultRegex;
-  export let key;
+  import { standardiseWithParen, standardiseWithoutParen } from "../../../data/standardiseName";
+  import { getColor } from "./colors";
+  import Location from "./Location";
 
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const params = Object.fromEntries(urlSearchParams.entries());
+  export let data;
+  export let params;
+  export let title;
+
+  let lookups = {};
+  let locationDict = {};
+  let locationList = [];
+
+  for (let [key, value] of data) {
+    // extract between brackets []
+    let locationSlug = key.match(/\[(.*?)\]/);
+    locationSlug = locationSlug ? locationSlug[1] : standardiseWithParen(key);
+
+    // extract anything after |
+    let spellings = key.match(/\|\w+/g);
+    spellings = spellings ? spellings.map((s => s.replace("|", ""))) : [standardiseWithoutParen(key).toLowerCase().replaceAll(" ", "")];
+
+    // before brackets
+    const cleanText = key.split("[").shift().trim();
+
+    for (let spelling of spellings) {
+      if (!lookups[spelling]) lookups[spelling] = [];
+      lookups[spelling].push(locationSlug); 
+    }
+
+    let cleanName = standardiseWithoutParen(key).toLowerCase().replaceAll(" ", "");
+    if (!lookups[cleanName]) lookups[cleanName] = [];
+    lookups[cleanName].push(locationSlug);
+
+    const location = new Location({
+      lat: value[1], lng: value[0], label: cleanText, color: (() => {
+        if (value.length > 2) return getColor("city", value[2]);
+        return getColor("mrt", cleanText);
+      })()
+    });
+
+    locationDict[locationSlug] = {
+      id: locationList.length,
+      name: cleanText
+    };
+    locationList.push(location);
+  }
 </script>
 
-<div class="max-w-3xl mx-auto p-2">
-  <InputBox answers={locationDict} defaultRegex={params.regex || defaultRegex} key={key}/>
-  <LLMap locations={locationList} defaultRegex={params.regex || defaultRegex}/>
+<div class="max-w-6xl mx-auto">
+  <InputBox answers={locationDict} {lookups} defaultRegex={params.selection || ""} isUntimed={params.isUntimed} {title}/>
+  <LLMap locations={locationList} defaultRegex={params.selection || ""} isUntimed={params.isUntimed}/>
 </div>
