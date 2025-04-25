2a88f81715b3684a664ddb6f60fc8ec71f8c5b67
Izz Hafeez <izzhafeez@gmail.com>
2025-04-25 16:20:08 +0800

    Reduce size of quiz selection

--- src/components/quizzes/QuizSelection.svelte
+++ src/components/quizzes/QuizSelection.svelte
@@ -5,6 +5,7 @@
     import homeData from "../../data/home.json";
     import convertSlug from "../../data/convert/convertSlug";
     import catGeoSlugs from "../../data/cat-geo-slugs.json";
+    import { capitalise } from "../../utils/string";
 
     // svg imports
     import logos from "../common/logos";
@@ -135,9 +136,9 @@
     <div class ="grid gap-3">
         <h2 class="text-3xl text-ns-500 font-extrabold">SEARCH RESULTS</h2>
         {#each finalSlugs as slug}
-            <a href={`/quizzes/${slug}`} class="text-lg hover:underline rounded-lg">
-                <div class="flex gap-4">
-                    <img src={logos[slug.split("-")[0]].src} class="w-10 h-10 my-auto" alt="svg"/><span class="bg-ns-500/50 p-2 rounded-lg">{convertSlug(slug).split(":")[0]}</span> <span class="my-auto">{convertSlug(slug).split(":")[1]}</span>
+            <a href={`/quizzes/${slug}`} class="lg:text-lg hover:underline rounded-lg">
+                <div class="flex gap-2 lg:gap-4">
+                    <img src={logos[slug.split("-")[0]].src} class="w-5 h-5 lg:w-10 lg:h-10 my-auto" alt="svg"/><span class="text-ns-500 lg:text-black lg:dark:text-white lg:bg-ns-500/50 lg:p-2 rounded-lg">{capitalise(slug.split("-")[0])}</span> <span class="my-auto">{convertSlug(slug).split(":")[1]}</span>
                 </div>
             </a>
         {/each}
